e3541c7a0f2d69d6e3f1d6deeb45f0721cdec30d
superorange0707 <77090682+superorange0707@users.noreply.github.com>
2026-08-28 12:11:19 +0100

    project-brain 0.9.1

--- Formula/project-brain.rb
+++ Formula/project-brain.rb
@@ -6,21 +6,21 @@ class ProjectBrain < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.0/project-brain-v0.9.0-macos-arm64.tar.gz"
-      sha256 "14fbf2014ff18c0b56eee2fdc514a0aca2fb59e50b18c52db43783371f1c86f9"
+      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.1/project-brain-v0.9.1-macos-arm64.tar.gz"
+      sha256 "0a39dc8da9b0bc85435d5217fd6f31b471a3bb879d761e5dc3ded5ec130c2dda"
     else
-      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.0/project-brain-v0.9.0-macos-amd64.tar.gz"
-      sha256 "415e1810ce3150aa851d6a2a09311371e256fa9c5a1bdea2fad41338e998469d"
+      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.1/project-brain-v0.9.1-macos-amd64.tar.gz"
+      sha256 "9c56d0971fe2582de05501ea58e7e6164370ec754e531ef0d90512150830dd13"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.0/project-brain-v0.9.0-linux-arm64.tar.gz"
-      sha256 "81c42573cd1a1b3e4bdfafc59e630c135d8495039222813a6832f8616e13e21f"
+      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.1/project-brain-v0.9.1-linux-arm64.tar.gz"
+      sha256 "fb399737a8ac85ce4cf74d05c9eda0ca389eb98509b7d72a0743ff53358d6452"
     else
-      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.0/project-brain-v0.9.0-linux-amd64.tar.gz"
-      sha256 "5cf9f4a15e0e3d8b8b5f37bfdf4b18f38c645194437095ef9a1cd445366a3a06"
+      url "https://github.com/superorange0707/project-brain/releases/download/v0.9.1/project-brain-v0.9.1-linux-amd64.tar.gz"
+      sha256 "3d9edc9126181d65bb5a412cb1e0262c866c147ce67be0ead0a253f8a66c77b5"
     end
   end
 
@@ -31,7 +31,7 @@ class ProjectBrain < Formula
   end
 
   test do
-    assert_match "brain 0.9.0", shell_output("#{bin}/brain --version")
+    assert_match "brain 0.9.1", shell_output("#{bin}/brain --version")
     assert_match "0.10.5", shell_output("#{bin}/codebase-memory-mcp --version 2>&1")
     assert_predicate bin/"zoekt", :executable?
     assert_predicate bin/"zoekt-index", :executable?
